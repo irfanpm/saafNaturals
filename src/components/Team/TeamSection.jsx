f88004c00dbd60820156139efa8d76bb4203f08c
irfanSuperio <irfan@superiodigital.com>
2025-04-23 17:13:17 +0530

    latest update

--- src/components/Team/TeamSection.jsx
+++ src/components/Team/TeamSection.jsx
@@ -17,7 +17,7 @@ const TeamSection = () => {
     {
       id: 2,
       name: 'Muhammed Ashraf pn',
-      role: 'CEO',
+      role: 'COO',
       image: '/saafImage/asru.jpg',
       bio: 'Financial strategist with a passion for eco-friendly business models, ensuring Saaf Natural’s growth and stability.',
       socialLinks: [
