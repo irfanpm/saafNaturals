5f1202190ac832004d0a69e8d60b4c5022cf207e
irfanSuperio <irfan@superiodigital.com>
2025-04-28 10:56:53 +0530

    latest update

--- src/components/SubHeading/index.jsx
+++ src/components/SubHeading/index.jsx
@@ -5,7 +5,6 @@ import styles from './styles.module.scss';
 const SubHeading = ({ title, image }) => (
   <div className={styles.sub_heading}>
     <p className='cormorant'>{title}</p>
-    <LineBreak />
   </div>
 );
 
